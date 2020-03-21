2f4253b22d7e8ba7dc9ff44df7742bb6d5f81c7e
Jacob Davis <ismyhc@gmail.com>
2020-03-21 14:06:51 -0400

    cleanup

--- Sources/Proton/Operations/FetchUserAccountInfoOperation.swift
+++ Sources/Proton/Operations/FetchUserAccountInfoOperation.swift
@@ -21,46 +21,6 @@ class FetchUserAccountInfoOperation: AbstractOperation {
     
     override func main() {
         
-//        let parameters = [
-//            "table": "usersinfo",
-//            "json": true,
-//            "lower_bound": self.account.name,
-//            "upper_bound": self.account.name,
-//            "scope": self.chainProvider.usersInfoTableScope,
-//            "code": self.chainProvider.usersInfoTableCode
-//            ] as [String : Any]
-//
-//        WebServices.shared.postRequestJSON(withPath: "\(chainProvider.chainUrl)/v1/chain/get_table_rows", parameters: parameters) { result in
-//
-//            switch result {
-//
-//            case .success(let retval):
-//
-//                guard let retval = retval as? [String: Any] else { return self.finish(retval: nil, error: WebServiceError.error("ERROR: No rows found")) }
-//                guard let rows = retval["rows"] as? [[String: Any]], rows.count > 0 else { return self.finish(retval: nil, error: WebServiceError.error("ERROR: No rows found")) }
-//                guard let firstRow = rows.first else { return self.finish(retval: nil, error: WebServiceError.error("ERROR: No rows found")) }
-//
-//                if let avatar = firstRow["avatar"] as? String {
-//                    self.account.base64Avatar = avatar
-//                }
-//
-//                if let name = firstRow["name"] as? String, !name.isEmpty {
-//                    self.account.fullName = name
-//                }
-//
-//                if let verified = firstRow["verified"] as? Bool {
-//                    self.account.verified = verified
-//                }
-//
-//                self.finish(retval: self.account, error: nil)
-//
-//            case .failure(let error):
-//                self.finish(retval: nil, error: WebServiceError.error(error.localizedDescription))
-//            }
-//
-//        }
-
-        
         guard let url = URL(string: chainProvider.chainUrl) else {
             self.finish(retval: nil, error: WebServiceError.error("ERROR: Missing url for get table rows"))
             return
@@ -87,7 +47,6 @@ class FetchUserAccountInfoOperation: AbstractOperation {
 
             let res = try client.sendSync(req).get()
 
-            print(res)
             if let userInfo = res.rows.first {
                 account.base64Avatar = userInfo.avatar ?? ""
                 account.fullName = userInfo.name ?? ""
