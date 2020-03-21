7054ac9d9d44c8516cc75234eecc66dbb30e3a0a
Jacob Davis <ismyhc@gmail.com>
2020-03-21 14:05:04 -0400

    fix account info

--- Sources/Proton/Operations/FetchUserAccountInfoOperation.swift
+++ Sources/Proton/Operations/FetchUserAccountInfoOperation.swift
@@ -21,79 +21,85 @@ class FetchUserAccountInfoOperation: AbstractOperation {
     
     override func main() {
         
-        let parameters = [
-            "table": "usersinfo",
-            "json": true,
-            "lower_bound": self.account.name,
-            "upper_bound": self.account.name,
-            "scope": self.chainProvider.usersInfoTableScope,
-            "code": self.chainProvider.usersInfoTableCode
-            ] as [String : Any]
-        
-        WebServices.shared.postRequestJSON(withPath: "\(chainProvider.chainUrl)/v1/chain/get_table_rows", parameters: parameters) { result in
-            
-            switch result {
-                
-            case .success(let retval):
-                
-                guard let retval = retval as? [String: Any] else { return self.finish(retval: nil, error: WebServiceError.error("ERROR: No rows found")) }
-                guard let rows = retval["rows"] as? [[String: Any]], rows.count > 0 else { return self.finish(retval: nil, error: WebServiceError.error("ERROR: No rows found")) }
-                guard let firstRow = rows.first else { return self.finish(retval: nil, error: WebServiceError.error("ERROR: No rows found")) }
-                
-                if let avatar = firstRow["avatar"] as? String {
-                    self.account.base64Avatar = avatar
-                }
-                
-                if let name = firstRow["name"] as? String, !name.isEmpty {
-                    self.account.fullName = name
-                }
-                
-                if let verified = firstRow["verified"] as? Bool {
-                    self.account.verified = verified
-                }
-                
-                self.finish(retval: self.account, error: nil)
-                
-            case .failure(let error):
-                self.finish(retval: nil, error: WebServiceError.error(error.localizedDescription))
-            }
-            
-        }
-
-        
-//        guard let url = URL(string: chainProvider.chainUrl) else {
-//            self.finish(retval: nil, error: WebServiceError.error("ERROR: Missing url for get table rows"))
-//            return
-//        }
-        
-//        struct UserInfo: ABICodable {
-//            let avatar: String?
-//            let name: String?
-//            let verified: Bool?
-//        }
+//        let parameters = [
+//            "table": "usersinfo",
+//            "json": true,
+//            "lower_bound": self.account.name,
+//            "upper_bound": self.account.name,
+//            "scope": self.chainProvider.usersInfoTableScope,
+//            "code": self.chainProvider.usersInfoTableCode
+//            ] as [String : Any]
 //
-//        let client = Client(address: url)
-//        var req = API.V1.Chain.GetTableRows<UserInfo>(code: Name(stringValue: chainProvider.usersInfoTableCode), table: Name(stringValue: "usersinfo"), scope: chainProvider.usersInfoTableScope)
-//        req.lowerBound = account.name
-//        req.upperBound = account.name
+//        WebServices.shared.postRequestJSON(withPath: "\(chainProvider.chainUrl)/v1/chain/get_table_rows", parameters: parameters) { result in
 //
-//        do {
+//            switch result {
 //
-//            let res = try client.sendSync(req).get()
+//            case .success(let retval):
 //
-//            print(res)
-//            if let userInfo = res.rows.first {
-//                account.base64Avatar = userInfo.avatar ?? ""
-//                account.fullName = userInfo.name ?? ""
-//                account.verified = userInfo.verified ?? false
+//                guard let retval = retval as? [String: Any] else { return self.finish(retval: nil, error: WebServiceError.error("ERROR: No rows found")) }
+//                guard let rows = retval["rows"] as? [[String: Any]], rows.count > 0 else { return self.finish(retval: nil, error: WebServiceError.error("ERROR: No rows found")) }
+//                guard let firstRow = rows.first else { return self.finish(retval: nil, error: WebServiceError.error("ERROR: No rows found")) }
+//
+//                if let avatar = firstRow["avatar"] as? String {
+//                    self.account.base64Avatar = avatar
+//                }
+//
+//                if let name = firstRow["name"] as? String, !name.isEmpty {
+//                    self.account.fullName = name
+//                }
+//
+//                if let verified = firstRow["verified"] as? Bool {
+//                    self.account.verified = verified
+//                }
+//
+//                self.finish(retval: self.account, error: nil)
+//
+//            case .failure(let error):
+//                self.finish(retval: nil, error: WebServiceError.error(error.localizedDescription))
 //            }
 //
-//            self.finish(retval: account, error: nil)
-//
-//        } catch {
-//            print("ERROR: \(error.localizedDescription)")
-//            self.finish(retval: nil, error: error)
 //        }
+
+        
+        guard let url = URL(string: chainProvider.chainUrl) else {
+            self.finish(retval: nil, error: WebServiceError.error("ERROR: Missing url for get table rows"))
+            return
+        }
+        
+        struct UserInfo: ABICodable {
+            let acc: Name
+            let name: String
+            let avatar: String
+            let verified: Bool
+            let date: UInt64
+            let data: String
+            let primary: Bool
+        }
+
+        let client = Client(address: url)
+        var req = API.V1.Chain.GetTableRows<UserInfo>(code: Name(stringValue: chainProvider.usersInfoTableCode),
+                                                      table: Name(stringValue: "usersinfo"),
+                                                      scope: chainProvider.usersInfoTableScope)
+        req.lowerBound = account.name
+        req.upperBound = account.name
+
+        do {
+
+            let res = try client.sendSync(req).get()
+
+            print(res)
+            if let userInfo = res.rows.first {
+                account.base64Avatar = userInfo.avatar ?? ""
+                account.fullName = userInfo.name ?? ""
+                account.verified = userInfo.verified ?? false
+            }
+
+            self.finish(retval: account, error: nil)
+
+        } catch {
+            print("ERROR: \(error.localizedDescription)")
+            self.finish(retval: nil, error: error)
+        }
         
     }
     
