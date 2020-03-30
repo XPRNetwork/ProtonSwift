99e44411fa786ad7996e197252fe20b4a194cad3
Jacob Davis <ismyhc@gmail.com>
2020-03-30 16:16:50 -0400

    debug

--- Sources/Proton/Operations/FetchTokenContractsOperation.swift
+++ Sources/Proton/Operations/FetchTokenContractsOperation.swift
@@ -37,35 +37,29 @@ class FetchTokenContractsOperation: AbstractOperation {
             
             for row in res.rows {
                 
-//                if var tokenContract = self.tokenContracts.first(where: { $0.contract.stringValue == row.tcontract.stringValue && $0.symbol.name == row.symbol.name }) {
-//
-//                    tokenContract.name = row.tname
-//                    tokenContract.url = row.url
-//                    tokenContract.description = row.desc
-//                    tokenContract.iconUrl = row.iconurl
-//                    tokenContract.blacklisted = row.blisted
-//
-//                    self.tokenContracts.update(with: tokenContract)
-//
-//                } else {
-//
-////                    let tokenContract = TokenContract(chainId: self.chainProvider.chainId, contract: row.tcontract,
-////                                                      issuer: row.tcontract, resourceToken: false, systemToken: false,
-////                                                      name: row.tname, description: row.desc, iconUrl: row.iconurl,
-////                                                      supply: Asset(0.0, row.symbol), maxSupply: Asset(0.0, row.symbol),
-////                                                      symbol: row.symbol, url: row.url, blacklisted: row.blisted)
-//
-//                    let tokenContract = TokenContract(chainId: "123", contract: Name(stringValue: "contract"),
-//                    issuer: Name(stringValue: "contract"), resourceToken: false, systemToken: false,
-//                    name: "name", description: "desc", iconUrl: "row.iconurl",
-//                    supply: Asset(0.0, try! Asset.Symbol(4, "EOS")), maxSupply: Asset(0.0, try! Asset.Symbol(4, "EOS")),
-//                    symbol: try! Asset.Symbol(4, "EOS"), url: "", blacklisted: false)
-//
-//
-//
-//                    self.tokenContracts.update(with: tokenContract)
-//
-//                }
+                if var tokenContract = self.tokenContracts.first(where: { $0.contract.stringValue == row.tcontract.stringValue && $0.symbol.name == row.symbol.name }) {
+                    
+                    tokenContract.name = row.tname
+                    tokenContract.url = row.url
+                    tokenContract.description = row.desc
+                    tokenContract.iconUrl = row.iconurl
+                    tokenContract.blacklisted = row.blisted
+                    
+                    self.tokenContracts.update(with: tokenContract)
+                    
+                } else {
+                    
+                    let tokenContract = TokenContract(chainId: self.chainProvider.chainId, contract: row.tcontract,
+                                                      issuer: row.tcontract, resourceToken: false, systemToken: false,
+                                                      name: row.tname, description: row.desc, iconUrl: row.iconurl,
+                                                      supply: Asset(0.0, row.symbol), maxSupply: Asset(0.0, row.symbol),
+                                                      symbol: row.symbol, url: row.url, blacklisted: row.blisted)
+                    
+                    print(tokenContract.description)
+                    
+                    self.tokenContracts.update(with: tokenContract)
+                    
+                }
                 
             }
 
