bd1098fecb28ecdfe1b2508faea7207da685437e
Jacob Davis <ismyhc@gmail.com>
2020-05-13 14:37:05 -0400

    fix transfer action other

--- Sources/Proton/Models/TokenTransferAction.swift
+++ Sources/Proton/Models/TokenTransferAction.swift
@@ -76,7 +76,7 @@ public struct TokenTransferAction: Codable, Identifiable, Hashable, ContactProto
     }
     
     public var other: Name {
-        return self.sent ? self.to : self.from
+        return self.sent ? self.from : self.to
     }
     
     public var contact: Contact? {
