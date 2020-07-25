856d99e9370ac7f778d7d362dcc44706922df14a
Jacob Davis <ismyhc@gmail.com>
2020-07-24 22:51:58 -0400

    debug url

--- Sources/Proton/Proton.swift
+++ Sources/Proton/Proton.swift
@@ -1397,7 +1397,7 @@ public class Proton {
                         for producer in producers {
                             //print(producer.url)
                             print(producer.owner.stringValue)
-                            print(producer.total_votes.value)
+                            print(producer.total_votes)
                             print(producer.url)
                         }
                         //print(producers)
